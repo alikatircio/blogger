e6c8200f462f770c79db9ca9f68dcb3fa62ac11d
alikatircioglu <ali.katircio@gmail.com>
2012-08-03 11:19:43 +0300

    blog oluşturuldu.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '2779174533b3bff421daf68f6035be37cd6319167618652c6e174874590b58a198e636c391618e28129ce57b5291af05560bf748c2d00ec78889f698579be11d'
